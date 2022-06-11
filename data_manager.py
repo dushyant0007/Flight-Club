ce055c50cb330ff70dd34dae98ecb972eb78a294
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-06-11 16:57:08 +0530

    Update data_manager.py

--- data_manager.py
+++ data_manager.py
@@ -1,21 +1,34 @@
-from pprint import pprint
+import os
 import requests
-
-SHEETY_PRICES_ENDPOINT = YOUR SHEETY PRICES ENDPOINT
 
 
 class DataManager:
-
+    """This class is responsible for talking to google sheets"""
     def __init__(self):
-        self.destination_data = {}
+        self.endpoint = os.environ.get("SHEETY_ENDPOINT")
+        self.username = os.environ.get("SHEETY_USERNAME")
+        self.password = os.environ.get("SHEETY_PASSWORD")
 
-    def get_destination_data(self):
-        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        # Destination Data
+        self.destination_data = {}
+        # Users data
+        self.users_data = {}
+
+    def get_destinations_data(self):
+        response = requests.get(url=f"{self.endpoint}/prices", auth=(self.username, self.password))
+        response.raise_for_status()
         data = response.json()
         self.destination_data = data["prices"]
         return self.destination_data
 
-    def update_destination_codes(self):
+    def get_users_data(self):
+        response = requests.get(url=f"{self.endpoint}/users", auth=(self.username, self.password))
+        response.raise_for_status()
+        data = response.json()
+        self.users_data = data["users"]
+        return self.users_data
+
+    def update_sheets_data(self):
         for city in self.destination_data:
             new_data = {
                 "price": {
@@ -23,7 +36,8 @@ class DataManager:
                 }
             }
             response = requests.put(
-                url=f"{SHEETY_PRICES_ENDPOINT}/{city['id']}",
-                json=new_data
+                url=f"{self.endpoint}/{city['id']}",
+                json=new_data,
+                auth=(self.username, self.password)
             )
             print(response.text)
