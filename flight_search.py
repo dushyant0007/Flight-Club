acbc5cbd3c759d3ed14f56ad313d23447eceeaed
Dushyant Jakhar <44732347+dushyant0007@users.noreply.github.com>
2022-06-11 16:58:05 +0530

    Update flight_search.py

--- flight_search.py
+++ flight_search.py
@@ -1,56 +1,100 @@
+import os
 import requests
-from flight_data import FlightData
-
-TEQUILA_ENDPOINT = "https://tequila-api.kiwi.com"
-TEQUILA_API_KEY = YOUR FLIGHT SEARCH API KEY
 
 
 class FlightSearch:
+    def __init__(self):
+        self.api_key = os.environ.get("TEQUILA_API_KEY")
 
-    def get_destination_code(self, city_name):
-        location_endpoint = f"{TEQUILA_ENDPOINT}/locations/query"
-        headers = {"apikey": TEQUILA_API_KEY}
-        query = {"term": city_name, "location_types": "city"}
-        response = requests.get(url=location_endpoint, headers=headers, params=query)
-        results = response.json()["locations"]
-        code = results[0]["code"]
-        return code
+        # location code endpoint
+        self.location_endpoint = "https://tequila-api.kiwi.com/locations/query"
 
-    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time):
-        headers = {"apikey": TEQUILA_API_KEY}
-        query = {
+        # flights search api
+        self.flights_search_api_endpoint = "https://tequila-api.kiwi.com/v2/search"
+
+        # Api authentication header data
+        self.headers = {
+            "apikey": self.api_key
+        }
+        self.location_parameters = {
+            "locale": "en-US",
+            "location_types": "airport",
+            "limit": 1,
+            "active_only": "true",
+        }
+
+    def get_iata_code(self, airport_name):
+        self.location_parameters["term"] = airport_name
+        response = requests.get(self.location_endpoint, params=self.location_parameters, headers=self.headers)
+        response.raise_for_status()
+        iata_code = response.json()["locations"][0]["id"]
+        return iata_code
+
+    def check_flights(self, origin_city_code, destination_city_code, from_time, to_time, curr):
+        api_parameters = {
             "fly_from": origin_city_code,
             "fly_to": destination_city_code,
             "date_from": from_time.strftime("%d/%m/%Y"),
             "date_to": to_time.strftime("%d/%m/%Y"),
             "nights_in_dst_from": 7,
             "nights_in_dst_to": 28,
-            "flight_type": "round",
-            "one_for_city": 1,
-            "max_stopovers": 0,
-            "curr": "GBP"
+            "flights_type": "round",
+            "curr": curr,
+            "max_stopovers": 0
         }
 
         response = requests.get(
-            url=f"{TEQUILA_ENDPOINT}/v2/search",
-            headers=headers,
-            params=query,
+            url=f"{self.flights_search_api_endpoint}",
+            params=api_parameters,
+            headers=self.headers
         )
-
+        response.raise_for_status()
+        # See if there are flights
         try:
             data = response.json()["data"][0]
+        # If there are no flights
         except IndexError:
-            print(f"No flights found for {destination_city_code}.")
-            return None
-
-        flight_data = FlightData(
-            price=data["price"],
-            origin_city=data["route"][0]["cityFrom"],
-            origin_airport=data["route"][0]["flyFrom"],
-            destination_city=data["route"][0]["cityTo"],
-            destination_airport=data["route"][0]["flyTo"],
-            out_date=data["route"][0]["local_departure"].split("T")[0],
-            return_date=data["route"][1]["local_departure"].split("T")[0]
-        )
-        print(f"{flight_data.destination_city}: £{flight_data.price}")
-        return flight_data
+            # Check if there are flights with stopovers
+            api_parameters["max_stopovers"] = 1
+            response = requests.get(
+                url=f"{self.flights_search_api_endpoint}",
+                params=api_parameters,
+                headers=self.headers
+            )
+            response.raise_for_status()
+            try:
+                # Try to see if there are flights with stopovers
+                data = response.json()["data"][0]
+            except IndexError:
+                # No flights even with stopovers
+                print(f"There are no flights for {destination_city_code}")
+                return None
+            else:
+                # returning flight data with stopovers
+                flight_data = {
+                    "price": data["price"],
+                    "origin_city": data["route"][0]["cityFrom"],
+                    "origin_airport": data["route"][0]["flyFrom"],
+                    "destination_city": data["route"][0]["cityTo"],
+                    "destination_airport": data["route"][0]["flyTo"],
+                    "travel_date": data["route"][0]["local_departure"].split("T")[0],
+                    "return_date": data["route"][1]["local_departure"].split("T")[0],
+                    "via_city": data['route'][0]['cityTo'],
+                    "stop_overs": 1
+                }
+                return flight_data
+        # If there is a flight with no stopovers
+        else:
+            # Flight data
+            flight_data = {
+                "price": data["price"],
+                "origin_city": data["route"][0]["cityFrom"],
+                "origin_airport": data["route"][0]["flyFrom"],
+                "destination_city": data["route"][0]["cityTo"],
+                "destination_airport": data["route"][0]["flyTo"],
+                "travel_date": data["route"][0]["local_departure"].split("T")[0],
+                "return_date": data["route"][1]["local_departure"].split("T")[0],
+                "via_city": "",
+                "stop_overs": 0
+            }
+            return flight_data
